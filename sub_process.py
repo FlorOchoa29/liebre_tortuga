df0b107ecde181f48e19472fa2bedc48fab365d7
FlorOchoa29 <oflor8718@gmail.com>
2026-03-23 12:52:04 -0600

    Avance

---
+++ sub_process.py
@@ -0,0 +1,39 @@
+import time
+from PyQt6.QtCore import QThread, pyqtSignal
+
+#Estaclase no sabe cual Label manipula, si la Liebre o la Tortuga,
+#Solo sabe que tiene que mover un label de un punto a otro
+#Y también recibe el tiempo que tarda en moverse (si es más rápido es la Liebre)
+
+class SubProceso(QThread):
+    actualizar = pyqtSignal(int,int)
+
+    def __init__(self, label, tiempo):
+        super().__init__()
+        self.label = label
+        self.tiempo = tiempo
+
+    def run(self):
+        while True:
+            x = self.label.x()
+            y = self.label.y()
+            #programar la lógica de la carrera aquí
+
+            # Imagen liebre
+            #self.actualizar.emit("img/liebre.png")
+            #self.sleep(3)
+
+            # Imagen tortuga
+            #self.actualizar.emit("img/tortuga.png")
+            #self.sleep(2)
+
+            #self.actualizar.emit("img/meta.png")
+            #self.sleep(3)
+
+            if x < 550:
+                x += 10
+            else:
+                y += 10
+
+            self.actualizar.emit(x,y)
+            time.sleep(self.tiempo / 1000.0)
---
+++ window.py
@@ -0,0 +1,54 @@
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel
+from PyQt6.QtGui import QPixmap
+from sub_process import SubProceso
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Liebre vs Tortuga")
+        self.setGeometry(100,100,800,400)
+
+        #Liebre
+        self.imgLiebre=QLabel(self)
+        self.imgLiebre.setPixmap(QPixmap("./img/liebre.png"))
+        self.imgLiebre.setScaledContents(True)
+        self.imgLiebre.resize(100,100)
+        self.imgLiebre.move(10,50)
+
+        #Tortuga
+        self.imgTortuga=QLabel(self)
+        self.imgTortuga.setPixmap(QPixmap("./img/tortuga.png"))
+        self.imgTortuga.setScaledContents(True)
+        self.imgTortuga.resize(100,100)
+        self.imgTortuga.move(10,150)
+
+        #Botón iniciar
+        self.btnIniciar = QPushButton("Iniciar", self)
+        self.btnIniciar.move(350,300)
+        self.btnIniciar.clicked.connect(self.iniciar)
+
+        #Crear subprocesos
+        self.pLiebre = SubProceso(self.imgLiebre,500)
+        self.pTortuga = SubProceso(self.imgTortuga,5000)
+
+        #Conectar señales
+        self.pLiebre.actualizar.connect(self.moverLiebre)
+        self.pTortuga.actualizar.connect(self.moverTortuga)
+
+    def iniciar(self):
+        if self.btnIniciar.text() == "Iniciar":
+            self.btnIniciar.setStyleSheet("background-color: green")
+            self.btnIniciar.setText("Detener")
+            self.pLiebre.start()
+            self.pTortuga.start()
+        else:
+            self.btnIniciar.setStyleSheet("background-color: red")
+            self.btnIniciar.setText("Iniciar")
+            self.pLiebre.stop()
+            self.pTortuga.stop()
+
+    def moverLiebre(self,x,y):
+        self.imgLiebre.move(x,y)
+    
+    def moverTortuga(self,x,y):
+        self.imgTortuga.move(x,y)
